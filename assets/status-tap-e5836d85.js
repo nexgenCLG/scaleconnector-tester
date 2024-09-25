5af52bbc67ccbd1ba0729ddda0d9962db887bf7d
nexgenCLG <claudia.gican@nexgen.ch>
2024-09-25 11:31:23 +0200

    Updates

--- assets/status-tap-e5836d85.js
+++ assets/status-tap-e5836d85.js
@@ -1,3 +1,3 @@
-import{r as a,a as i,b as c}from"./index-c870a356.js";import{f as d,s as l}from"./index8-d04a22e8.js";/*!
+import{r as a,a as i,b as c}from"./index-12400a9c.js";import{f as d,s as l}from"./index8-96a9038f.js";/*!
  * (C) Ionic http://ionicframework.com - MIT License
  */const w=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const n=e.innerWidth,s=e.innerHeight,o=document.elementFromPoint(n/2,s/2);if(!o)return;const t=d(o);t&&new Promise(r=>i(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{w as startStatusTap};
